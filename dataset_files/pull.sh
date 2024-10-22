c00059939c44af9e92b80442361e6eaba368b097
Markos Markakis <markakis@mit.edu>
2024-10-22 11:53:40 -0400

    For scaling evaluation, move raw results to `dataset_files`

--- dataset_files/pull.sh
+++ dataset_files/pull.sh
@@ -1,4 +1,4 @@
 aws s3 sync s3://logos-dataset-postgresql postgresql/  --delete --exclude "repro_evaluation/*"
 aws s3 sync s3://logos-dataset-proprietary proprietary/ --delete --exclude "repro_evaluation/*"
 aws s3 sync s3://logos-dataset-xyz xyz/ --delete --exclude "repro_evaluation/*"
-aws s3 sync s3://logos-dataset-scaling scaling/ --delete 
+aws s3 sync s3://logos-dataset-scaling scaling/ --delete --exclude "repro_evaluation/*"
--- dataset_files/push.sh
+++ dataset_files/push.sh
@@ -2,4 +2,4 @@
 aws s3 sync postgresql/ s3://logos-dataset-postgresql --delete --exclude "datasets_raw/*" --exclude "repro_evaluation/*"
 aws s3 sync proprietary/ s3://logos-dataset-proprietary --delete --exclude "datasets_raw/*" --exclude "repro_evaluation/*"
 aws s3 sync xyz/ s3://logos-dataset-xyz --delete --exclude "datasets_raw/*" --exclude "repro_evaluation/*"
-aws s3 sync scaling/ s3://logos-dataset-scaling --delete 
+aws s3 sync scaling/ s3://logos-dataset-scaling --delete --exclude "datasets_raw/*" --exclude "repro_evaluation/*"
