33639af7ed3d6d6259289b65d58ec6fd1a41922f
Markos Markakis <mmarkakis98@gmail.com>
2026-07-31 14:48:02 -0400

    Update pipfile etc

---
+++ src/logos/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+if sys.version_info < (3, 11):
+    raise RuntimeError(
+        f"LOGos requires Python 3.11 or later (running "
+        f"{sys.version_info.major}.{sys.version_info.minor})."
+    )
